44f84fffa1a4d2db7fe7c0b99d38e5ee93f03cfb
defoltusername <mosyanarayik777@gmail.com>
2026-05-18 17:17:16 +0400

    dsadsd

---
+++ classroom/react/src/components/Counter/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    const plus = () => {
+        setCount(count + 1)
+    }
+    const minus = () => {
+        setCount(count - 1)
+    }
+  return (
+    <div>
+      <h1>Count: {count} </h1>
+      <div>
+         <button onClick={plus}>+</button>
+         <button onClick={minus}>-</button>
+       
+      </div>
+    </div>
+  )
+}
+
+export default Counter
